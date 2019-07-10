6489f55704e8d341d6f2fbc40b8a20b9f20ea0cd
System Administrator <root@por-doeschea-m.ds.ad.adp.com>
2019-07-10 14:31:53 -0700

    Added CDK deployment support

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,34 @@
-FROM node:8
+FROM artifactory.cobalt.com/node8
 
-ADD yarn.lock /yarn.lock
-ADD package.json /package.json
+USER root
 
-ENV NODE_PATH=/node_modules
-ENV PATH=$PATH:/node_modules/.bin
-RUN yarn
+WORKDIR /usr/src/app
 
-WORKDIR /app
-ADD . /app
+COPY package*.json ./
 
+RUN npm install
+
+COPY . .
+
+RUN npm run-script build
+
+WORKDIR /usr/src/app
+
+USER default
 EXPOSE 3000
-EXPOSE 35729
-
-ENTRYPOINT ["/bin/bash", "/app/run.sh"]
-CMD ["start"]
+CMD ["node", "server.js"]
+#ADD package-lock.json /package-lock.json
+#ADD package.json /package.json
+#
+#ENV NODE_PATH=/node_modules
+#ENV PATH=$PATH:/node_modules/.bin
+#RUN npm install
+#
+#WORKDIR /app
+#ADD . /app
+#
+#EXPOSE 3000
+#EXPOSE 35729
+#
+#ENTRYPOINT ["/bin/bash"]
+#CMD ["start"]
---
+++ server.js
@@ -0,0 +1,59 @@
+
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+let express = require('express'); // call express
+let app = express(); // define our app using express
+
+let path = require('path');
+
+app.use(express.static(path.join(__dirname, '/build')));
+
+let port = process.env.PORT || 3000; // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+let router = express.Router(); // get an instance of the express Router
+
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // This is a place where we could write code to be handled for all requests
+    // such as checking for errors or checking for authentication
+    // do logging
+    next(); // make sure we go to the next routes and don't stop here
+});
+
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
+
+router.get('/health', function(req, res) {
+    res.json({ status: 'UP' });
+});
+
+router.get('/info', function(req, res) {
+    let pjson = require('package.json');
+    let deployEnv = process.env.DEPLOYMENT_ENV;
+
+    if (!deployEnv) {
+        deployEnv = 'unknown';
+        console.log('Unknown deployment environment');
+    }
+
+    console.log(pjson.name + ' v' + pjson.version + ' running in ' + deployEnv);
+    res.json({ name: pjson.name, version: pjson.version, deploy_env: deployEnv });
+});
+
+// REGISTER OUR ROUTES -------------------------------
+app.use('/', router);
+
+// START THE SERVER
+// =============================================================================
+
+let server = app.listen(port, function() {
+    console.log('Server running on port: ' + port);
+});
+
+module.exports = server;
