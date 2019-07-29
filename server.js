5e008a05469d672e9a1285ceb94a8c29b42f298c
Doescher, Aracelli <Aracelli.Doescher@cdk.com>
2019-07-29 15:26:22 -0700

    Please don't work

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ router.get('/dealerships', function(req, res) {
     res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
-router.get('/dealerships/:name', function(req, res) {
+router.get('/dealerships/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/build/index.html'));
 });
 
