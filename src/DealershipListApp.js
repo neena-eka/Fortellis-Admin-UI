1d28580167745b5bf7b55305119d8a1a98824c93
Doescher, Aracelli <Aracelli.Doescher@cdk.com>
2019-07-29 14:07:27 -0700

    Added list of dealership names to /dealerships, maybe fixed routing

--- src/DealershipListApp.js
+++ src/DealershipListApp.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import {DealershipListDisplay} from './DealershipListDisplay';
+import {fetchEntityInfo} from "./entityInfoAPIAccessor";
+import './DealershipListApp.css';
 
-function DealershipListApp(props) {
-    return (
-        <div className="DealershipListApp">
-            <header className="DealershipListApp-header">
-                <DealershipListDisplay url={props.match.url} />
-            </header>
-        </div>
-    )
+class DealershipListApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {dealershipNameData: []};
+        this.fetchNameInfo = this.fetchNameInfo.bind(this);
+    }
+
+    async fetchNameInfo() {
+        let attributes = await fetchEntityInfo();
+        let names = [];
+        let urlNames = [];
+        let dealershipNameData = [];
+        let dealershipName = '';
+        let urlName = '';
+        let item;
+        for (let i = 0; i < attributes.items.length; i++) {
+            item = attributes.items[i];
+            dealershipName = item.name.s;
+            if (!names.includes(dealershipName)) {
+                names.push(dealershipName);
+                urlName = '/' + dealershipName.toLowerCase();
+                while(urlName.includes(' ')) {
+                    urlName = urlName.replace(' ', '-');
+                }
+                urlNames.push(urlName);
+            }
+        }
+        for(let i = 0; i < names.length; i++) {
+            dealershipNameData.push(urlNames[i] + '||' + names[i]);
+        }
+        this.setState({dealershipNameData})
+    }
+
+    render() {
+        this.fetchNameInfo();
+        return (
+            <div className="DealershipListApp">
+                <header className="DealershipListApp-header">
+                    <DealershipListDisplay dealershipNameData={this.state.dealershipNameData}/>
+                </header>
+            </div>
+        )
+    }
 }
 export default DealershipListApp;
--- src/DealershipListDisplay.js
+++ src/DealershipListDisplay.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Nav from "react-bootstrap/Nav";
+import {pathURL} from "./index";
+import './DealershipListApp.css'
 
 export const DealershipListDisplay = (props) => {
+    /*const navLinks = props.urlNames.map(url => {
+        return <Nav.Link href={url}> {url.toUpperCase().replace('-', ' ').substring(1)} </Nav.Link>;
+    });*/
+
     return (
         <div className="dealership-display">
             <h1>Dealerships</h1>
             <Nav className="flex-column">
-                <Nav.Link href={`${props.url}/hi`}>{props.url}</Nav.Link>
-                <Nav.Link href="/">Dealerships</Nav.Link>
-                <Nav.Link href="/requests">Hi</Nav.Link>
-                <Nav.Link href="/requests">Requests</Nav.Link>
-                <Nav.Link href="/requests">Hello</Nav.Link>
+                {props.dealershipNameData.map(data => <Nav.Link href={pathURL + 'dealerships' + data.substring(0, data.indexOf('||'))}>{data.substring(data.indexOf('||') + 2)}</Nav.Link>)}
             </Nav>
-
         </div>
     )
 }
